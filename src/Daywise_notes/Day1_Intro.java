9c78879afb26dcef93bb227493183bb638af04fc
Vijay <vijayguru27@gmail.com>
2026-09-20 20:00:35 +0530

    Jave notes

---
+++ src/Daywise_notes/Day1_Intro.java
@@ -0,0 +1,7 @@
+package Daywise_notes;
+
+public class Day1_Intro {
+    public static void main(String[] args){
+        System.out.println("Hello I am Java");
+    }
+}
---
+++ src/Daywise_notes/Day2_DataTypes.java
@@ -0,0 +1,56 @@
+package Daywise_notes;
+
+public class Day2_DataTypes {
+    public static void main(String[] args){
+        //num
+        byte a = -128;
+        byte b = 10;
+        byte c = 127;
+        //byte d = 128; int number too large, limit is 127
+        //short
+        short e = -32768;
+        short f = 32767;
+
+        //int
+        int g = -2147483648;
+        int h = 2147483647;
+
+        //long
+        long i = -922L;
+        long j = 922l;
+
+        byte age = 40;
+        short year = 2000;
+        int pincode = 600001;
+        long phone = 987412563L;
+
+        //decimal
+
+        //float price = 45.26; //incompatible type, corrected type given below
+        float price = 45.26f;
+        float price1 = 45.26F;
+        float price2 = 454332232.33f;
+        double order_cost = 459896.23;
+        double order_value = 459986.23200000245;
+
+        //char
+
+        char suffix = 'm';
+        char prefix = 's';
+        char space = ' ';
+
+        //boolean
+
+        boolean is_jave = true;
+        boolean is_python = false;
+
+        //Collection of char
+
+        String name = "Vijay";
+
+
+
+
+
+    }
+}
